39a5fb7fa6cb2c6551a6ca22abb74dafcc58acab
unknown <s0936100879@gmail.com>
2020-07-29 23:58:01 +0800

    add_utils

--- utils/HardSampleMining.py
+++ utils/HardSampleMining.py
@@ -1,5 +1,17 @@
-# Assign Positive and negative by IOU thresholds
+import numpy as np
+import torch
 
-# Matching the best IOU candidated predicted samples with groundtruth
+
+# Assign positive and negative by IOU thresholds
+def Define_PN_with_DefaultAnchor_by_IOU(default_bboxes, gt_bboxes, IOU_thresh = 0.5):
+    default_labels = None
+    return default_labels
+
+# Matching the best IOU candidate predicted samples with default_bboxes
+def Match_predicted_and_default_bboxes_by_maxIOU():
+    pass
 
 # HardSampleBalanceMining
+def HardSampleMining_by_balancing_PN():
+    pass
+
